8353e6146f6a7d2b1b2450b3dd1bfecefab29dc0
arbab arshad <arshadsarhan786@gmail.com>
2025-10-07 20:52:27 +0530

    Fixed Bux

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import tailwind from '@tailwindcss/vite'
 
 
 export default defineConfig({
-  plugins: [react() , tailwindcss(),],
+  plugins: [react() , tailwindcss(),tailwind()],
   server: {
     port: 5173,
     proxy: {
